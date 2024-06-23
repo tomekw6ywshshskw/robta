4b8bad494332874d03686f9a1e57093f3776f334
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-06-23 21:22:26 +0200

    Update client

---
+++ core/client/Maine.lua
@@ -0,0 +1,5 @@
+-- Example client/main.lua
+addEventHandler("onClientResourceStart", resourceRoot, function()
+    -- Twój kod inicjalizacyjny po stronie klienta
+    outputChatBox("Hello from My Core client side!")
+end)
