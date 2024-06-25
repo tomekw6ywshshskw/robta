d01b46dd6d3834c7862c36f21775f01595b82075
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-06-25 07:28:55 +0200

    Update server.lua

--- licznik/server.lua
+++ licznik/server.lua
@@ -1,1 +1,10 @@
-..
+addEventHandler("onVehicleEnter", root, function(player, seat)
+    if seat == 0 then -- Tylko kierowca
+        if not getElementData(source, "vehicle:fuel") then
+            setElementData(source, "vehicle:fuel", 100) -- Początkowa ilość paliwa
+        end
+        if not getElementData(source, "vehicle:mileage") then
+            setElementData(source, "vehicle:mileage", 0) -- Początkowy przebieg
+        end
+    end
+end)
