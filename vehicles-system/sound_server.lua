84ec0767bc196f2d88138e91b80a62c225e6d700
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-07-20 23:10:55 +0200

    Update sound_server.lua

--- vehicles-system/sound_server.lua
+++ vehicles-system/sound_server.lua
@@ -1,14 +1,30 @@
--- Funkcja sprawdzająca, czy pojazd to muscle car
-function isMuscleCar(model)
-    local muscleCarIDs = {
-        400, 601, 426, 487, 484, 470, 603, 566
-    }
-    for _, id in ipairs(muscleCarIDs) do
-        if model == id then
-            return true
-        end
+-- Tabela z dźwiękami dla różnych kategorii
+local engineSounds = {
+    ["Sport cars"] = "sound/sport_engine_start.wav",
+    ["Muscle cars"] = "sound/muscle_engine_start.wav",
+    ["Off road"] = "sound/offroad_engine_start.wav",
+    ["Lowrider"] = "sound/lowrider_engine_start.wav",
+    ["Truck"] = "sound/truck_engine_start.wav",
+    ["Regular"] = "sound/regular_engine_start.wav"
+}
+
+-- Funkcja zwracająca kategorię pojazdu
+function getVehicleCategory(vehicle)
+    local model = getElementModel(vehicle)
+    
+    if isSportCar(model) then
+        return "Sport cars"
+    elseif isMuscleCar(model) then
+        return "Muscle cars"
+    elseif isOffRoadCar(model) then
+        return "Off road"
+    elseif isLowriderCar(model) then
+        return "Lowrider"
+    elseif isTruck(model) then
+        return "Truck"
+    else
+        return "Regular"
     end
-    return false
 end
 
 -- Funkcja ustawiająca dźwięk silnika
@@ -16,21 +32,12 @@ function setEngineSound(vehicle, player)
     local category = getVehicleCategory(vehicle)
     local useCustomSounds = playerCustomSounds[player] ~= false -- domyślnie włączone
     local soundFile = useCustomSounds and engineSounds[category] or "standard"
+    
+    -- Przesyłamy ustawienie dźwięku do klienta
     triggerClientEvent(player, "onClientSetEngineSound", player, soundFile, category)
 end
 
--- Komendy do włączania/wyłączania niestandardowych dźwięków
-addCommandHandler("disablecustomsounds", function(player)
-    playerCustomSounds[player] = false
-    outputChatBox("Custom vehicle sounds have been disabled. Default sounds will now be used.", player)
-end)
-
-addCommandHandler("enablecustomsounds", function(player)
-    playerCustomSounds[player] = true
-    outputChatBox("Custom vehicle sounds have been enabled.", player)
-end)
-
--- Event do ustawienia dźwięku silnika przy wsiadaniu do pojazdu
+-- Funkcja ustawiająca dźwięk silnika po wsiadaniu do pojazdu
 addEventHandler("onVehicleStartEnter", root, function(vehicle, player)
     setEngineSound(vehicle, player)
 end)
