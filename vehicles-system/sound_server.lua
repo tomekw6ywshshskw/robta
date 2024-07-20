8fae67d961bf16edc1c87bf6c6dc4bd24b888130
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-07-20 23:11:58 +0200

    Update sound_server.lua

--- vehicles-system/sound_server.lua
+++ vehicles-system/sound_server.lua
@@ -1,4 +1,4 @@
--- Tabela z dźwiękami dla różnych kategorii
+-- Tabela z niestandardowymi dźwiękami dla różnych kategorii
 local engineSounds = {
     ["Sport cars"] = "sound/sport_engine_start.wav",
     ["Muscle cars"] = "sound/muscle_engine_start.wav",
@@ -7,6 +7,71 @@ local engineSounds = {
     ["Truck"] = "sound/truck_engine_start.wav",
     ["Regular"] = "sound/regular_engine_start.wav"
 }
+
+-- Funkcja sprawdzająca, czy pojazd to muscle car
+function isMuscleCar(model)
+    local muscleCarIDs = {
+        400, 601, 426, 487, 484, 470, 603, 566
+    }
+    for _, id in ipairs(muscleCarIDs) do
+        if model == id then
+            return true
+        end
+    end
+    return false
+end
+
+-- Funkcja sprawdzająca, czy pojazd to sport car
+function isSportCar(model)
+    local sportCarIDs = {
+        411, 401, 560, 420, 556, 560, 429, 543
+    }
+    for _, id in ipairs(sportCarIDs) do
+        if model == id then
+            return true
+        end
+    end
+    return false
+end
+
+-- Funkcja sprawdzająca, czy pojazd to off road
+function isOffRoadCar(model)
+    local offRoadIDs = {
+        446, 448, 523, 472, 500
+    }
+    for _, id in ipairs(offRoadIDs) do
+        if model == id then
+            return true
+        end
+    end
+    return false
+end
+
+-- Funkcja sprawdzająca, czy pojazd to lowrider
+function isLowriderCar(model)
+    local lowriderIDs = {
+        516, 567, 517, 515, 426
+    }
+    for _, id in ipairs(lowriderIDs) do
+        if model == id then
+            return true
+        end
+    end
+    return false
+end
+
+-- Funkcja sprawdzająca, czy pojazd to truck
+function isTruck(model)
+    local truckIDs = {
+        464, 514, 486, 515, 421
+    }
+    for _, id in ipairs(truckIDs) do
+        if model == id then
+            return true
+        end
+    end
+    return false
+end
 
 -- Funkcja zwracająca kategorię pojazdu
 function getVehicleCategory(vehicle)
