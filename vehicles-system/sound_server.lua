cc3734c0c693b5419e93cd7be49508ad30fed9e6
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-07-20 23:09:01 +0200

    Update sound_server.lua

--- vehicles-system/sound_server.lua
+++ vehicles-system/sound_server.lua
@@ -1,36 +1,36 @@
--- Globalna tabela do przechowywania ustawień dla graczy
-local playerCustomSounds = {}
+-- Funkcja sprawdzająca, czy pojazd to muscle car
+function isMuscleCar(model)
+    local muscleCarIDs = {
+        400, 601, 426, 487, 484, 470, 603, 566
+    }
+    for _, id in ipairs(muscleCarIDs) do
+        if model == id then
+            return true
+        end
+    end
+    return false
+end
 
 -- Funkcja ustawiająca dźwięk silnika
 function setEngineSound(vehicle, player)
     local category = getVehicleCategory(vehicle)
     local useCustomSounds = playerCustomSounds[player] ~= false -- domyślnie włączone
-
-    local soundFile
-    if useCustomSounds then
-        soundFile = engineSounds[category]
-    else
-        -- Ustawienie dźwięku silnika na standardowy
-        soundFile = "standard"  -- Specjalne oznaczenie dla standardowych dźwięków
-    end
-    
-    -- Przesyłamy ustawienie dźwięku do klienta
+    local soundFile = useCustomSounds and engineSounds[category] or "standard"
     triggerClientEvent(player, "onClientSetEngineSound", player, soundFile, category)
 end
 
--- Komenda do wyłączenia niestandardowych dźwięków
+-- Komendy do włączania/wyłączania niestandardowych dźwięków
 addCommandHandler("disablecustomsounds", function(player)
     playerCustomSounds[player] = false
     outputChatBox("Custom vehicle sounds have been disabled. Default sounds will now be used.", player)
 end)
 
--- Komenda do włączenia niestandardowych dźwięków
 addCommandHandler("enablecustomsounds", function(player)
     playerCustomSounds[player] = true
     outputChatBox("Custom vehicle sounds have been enabled.", player)
 end)
 
--- Funkcja zarządzająca zmianą dźwięków po wsiadaniu do pojazdu
+-- Event do ustawienia dźwięku silnika przy wsiadaniu do pojazdu
 addEventHandler("onVehicleStartEnter", root, function(vehicle, player)
     setEngineSound(vehicle, player)
 end)
