a883a6cb291f541b46e0628b1b53f7ad517de27e
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-07-20 19:39:26 +0200

    Update client.lua

--- new-licznik/client.lua
+++ new-licznik/client.lua
@@ -1,3 +1,4 @@
+
 local screenWidth, screenHeight = guiGetScreenSize()
 local scale = screenWidth / 1920
 local font = "default-bold"
@@ -6,22 +7,24 @@ local showMiniMap = false
 function drawSpeedometer()
     local vehicle = getPedOccupiedVehicle(localPlayer)
     if vehicle then
+        -- Tło licznika
+        dxDrawImage(screenWidth / 2 - 150 * scale, screenHeight - 150 * scale, 300 * scale, 150 * scale, "images/background.png")
+
         -- Nazwa pojazdu na górze
         local vehicleName = getVehicleName(vehicle)
-        dxDrawText(vehicleName, screenWidth / 2, screenHeight - 170 * scale, screenWidth / 2, screenHeight - 170 * scale, tocolor(255, 255, 255), 1.5 * scale, font, "center", "top")
+        dxDrawText(vehicleName, screenWidth / 2, screenHeight - 145 * scale, screenWidth / 2, screenHeight - 145 * scale, tocolor(255, 255, 255), 1.5 * scale, font, "center", "top")
 
         -- Cyfrowy prędkościomierz
         local speed = getSpeed(vehicle)
         local color = getSpeedColor(speed)
-        dxDrawText(string.format("%d km/h", speed), screenWidth / 2, screenHeight - 140 * scale, screenWidth / 2, screenHeight - 140 * scale, color, 2 * scale, font, "center", "top")
+        dxDrawText(string.format("%d km/h", speed), screenWidth / 2, screenHeight - 115 * scale, screenWidth / 2, screenHeight - 115 * scale, color, 2 * scale, font, "center", "top")
 
         -- Poziom paliwa
         local fuel = getElementData(vehicle, "vehicle:fuel")
         dxDrawImage(screenWidth / 2 + 70 * scale, screenHeight - 90 * scale, 20 * scale, 20 * scale, "images/fuel_icon.png")
         dxDrawText(string.format("Fuel: %.1f L", fuel), screenWidth / 2 + 100 * scale, screenHeight - 90 * scale, screenWidth / 2 + 100 * scale, screenHeight - 90 * scale, tocolor(255, 255, 255), 1 * scale, font, "left", "top")
 
-        -- Realistyczna mini mapa na dole
-        dxDrawRectangle(screenWidth / 2 - 150 * scale, screenHeight - 60 * scale, 300 * scale, 50 * scale, tocolor(0, 0, 0, 150))
+        -- Mini mapa na dole
         drawMiniMap(screenWidth / 2 - 145 * scale, screenHeight - 55 * scale, 290 * scale, 40 * scale)
     else
         drawStandardRadar()
@@ -31,7 +34,7 @@ addEventHandler("onClientRender", root, drawSpeedometer)
 
 function drawMiniMap(x, y, width, height)
     -- Rysowanie realistycznej mini mapy
-    dxDrawImage(x, y, width, height, "images/mini_map.png")
+    dxDrawImage(x, y, width, height, "images/map.png")
 end
 
 function drawStandardRadar()
