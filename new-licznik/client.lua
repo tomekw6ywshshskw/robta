0dddcb4dd3ec61c5ed641fc6bea2f4b0a5cdeeb9
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-07-21 12:45:40 +0200

    Update client.lua

--- new-licznik/client.lua
+++ new-licznik/client.lua
@@ -3,32 +3,35 @@ local scale = screenWidth / 1920
 local font = "default-bold"
 local showMiniMap = false
 
+local backgroundImg = "images/background.png"
+local mapImg = "images/map.png"
+
 function drawSpeedometer()
     local vehicle = getPedOccupiedVehicle(localPlayer)
     if vehicle then
         -- Tło licznika
-        dxDrawImage(screenWidth / 2 - 150 * scale, screenHeight - 150 * scale, 300 * scale, 150 * scale, "images/background.png")
+        dxDrawImage(screenWidth / 2 - 300 * scale, screenHeight - 200 * scale, 600 * scale, 200 * scale, backgroundImg)
 
         -- Nazwa pojazdu na górze
         local vehicleName = getVehicleName(vehicle)
-        dxDrawText(vehicleName, screenWidth / 2, screenHeight - 145 * scale, screenWidth / 2, screenHeight - 145 * scale, tocolor(255, 255, 255), 1.5 * scale, font, "center", "top")
+        dxDrawText(vehicleName, screenWidth / 2, screenHeight - 195 * scale, screenWidth / 2, screenHeight - 195 * scale, tocolor(255, 255, 255), 1.5 * scale, font, "center", "top")
 
         -- Cyfrowy prędkościomierz
         local speed = getSpeed(vehicle)
         local color = getSpeedColor(speed)
-        dxDrawText(string.format("%d km/h", speed), screenWidth / 2, screenHeight - 115 * scale, screenWidth / 2, screenHeight - 115 * scale, color, 2 * scale, font, "center", "top")
+        dxDrawText(string.format("%d km/h", speed), screenWidth / 2, screenHeight - 120 * scale, screenWidth / 2, screenHeight - 120 * scale, color, 2 * scale, font, "center", "top")
 
         -- Poziom paliwa
         local fuel = getElementData(vehicle, "vehicle:fuel") or 0
-        dxDrawImage(screenWidth / 2 + 70 * scale, screenHeight - 90 * scale, 30 * scale, 30 * scale, "images/fuel_icon.png")
-        dxDrawText(string.format("Fuel: %.1f L", fuel), screenWidth / 2 + 105 * scale, screenHeight - 85 * scale, screenWidth / 2 + 105 * scale, screenHeight - 85 * scale, tocolor(255, 255, 255), 1 * scale, font, "left", "top")
+        dxDrawImage(screenWidth / 2 - 290 * scale, screenHeight - 100 * scale, 30 * scale, 30 * scale, "images/fuel_icon.png")
+        dxDrawText(string.format("Fuel: %.1f L", fuel), screenWidth / 2 - 255 * scale, screenHeight - 95 * scale, screenWidth / 2 - 255 * scale, screenHeight - 95 * scale, tocolor(255, 255, 255), 1 * scale, font, "left", "top")
 
         -- Przebieg pojazdu
         local mileage = getElementData(vehicle, "vehicle:mileage") or 0
-        dxDrawText(string.format("Mileage: %d km", mileage), screenWidth / 2, screenHeight - 60 * scale, screenWidth / 2, screenHeight - 60 * scale, tocolor(255, 255, 255), 1 * scale, font, "center", "top")
+        dxDrawText(string.format("Mileage: %d km", mileage), screenWidth / 2 + 150 * scale, screenHeight - 95 * scale, screenWidth / 2 + 150 * scale, screenHeight - 95 * scale, tocolor(255, 255, 255), 1 * scale, font, "left", "top")
 
         -- Mini mapa na dole
-        drawMiniMap(screenWidth / 2 - 145 * scale, screenHeight - 55 * scale, 290 * scale, 40 * scale)
+        drawMiniMap(screenWidth / 2 - 295 * scale, screenHeight - 80 * scale, 590 * scale, 60 * scale)
     else
         drawStandardRadar()
     end
@@ -37,7 +40,14 @@ addEventHandler("onClientRender", root, drawSpeedometer)
 
 function drawMiniMap(x, y, width, height)
     -- Rysowanie realistycznej mini mapy
-    dxDrawImage(x, y, width, height, "images/map.png")
+    local playerX, playerY, playerZ = getElementPosition(localPlayer)
+    local mapWidth, mapHeight = 6000, 6000 -- Zakładamy, że mapa ma rozmiar 6000x6000 jednostek
+    local mapScale = width / mapWidth
+    local mapX = x - (playerX * mapScale)
+    local mapY = y - (playerY * mapScale)
+    
+    dxDrawImage(mapX, mapY, mapWidth * mapScale, mapHeight * mapScale, mapImg)
+    dxDrawRectangle(x + (width / 2) - 5 * scale, y + (height / 2) - 5 * scale, 10 * scale, 10 * scale, tocolor(255, 0, 0, 255)) -- Pozycja gracza
 end
 
 function drawStandardRadar()
